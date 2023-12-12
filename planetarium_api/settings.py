b4f6650749a315e4401e24704d99a93b9e9a4406
D1st3f <dima.purig@gmail.com>
2023-12-12 11:07:51 +0200

    Prepare to deploy

--- planetarium_api/settings.py
+++ planetarium_api/settings.py
@@ -1,16 +1,15 @@
-import os
 from datetime import timedelta
 from pathlib import Path
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    "django-insecure-)6sjaa^)=qg6&p#5^)0#ns0db0r3#*5#v5f83-_c@-y^lx-p%&"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "newspapersagency-w41c.onrender.com"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -28,6 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -58,13 +58,13 @@ WSGI_APPLICATION = "planetarium_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ["POSTGRES_HOST"],
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+db_from_wnv = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_wnv)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,11 +100,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
